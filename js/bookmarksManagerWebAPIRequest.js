258a235d5a03841150a20bbcb4ea100181ea209a
Zacharie Lassey <201575553@edu.clg.qc.ca>
2021-10-08 23:14:00 -0400

    first commit

---
+++ js/bookmarksManagerWebAPIRequest.js
@@ -0,0 +1,119 @@
+const apiBaseURL= "http://localhost:5000/api/bookmarks";
+//const apiBaseURL= "https://three-ethereal-castanet.glitch.me/api/bookmarks";
+
+function webAPI_getBookmarks( successCallBack, errorCallBack) {
+    
+    $.ajax({
+        url: apiBaseURL,
+        type: 'GET',
+        contentType:'text/plain',
+        data:{},
+        success: bookmarks => {  
+                                
+                                successCallBack(bookmarks);
+                                
+                                console.log("webAPI_getBookmarks - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            errorCallBack(errorThrown);
+            console.log("webAPI_getBookmarks - error");
+        }
+    });
+}
+
+function webAPI_getBookmarksFiltered( successCallBack, errorCallBack) {
+    
+    $.ajax({
+        url: apiBaseURL+ "?sort=category",
+        type: 'GET',
+        contentType:'text/plain',
+        data:{},
+        success: bookmarks => {  
+                                
+                                successCallBack(bookmarks);
+                                
+                                console.log("webAPI_getBookmarks - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            errorCallBack(errorThrown);
+            console.log("webAPI_getBookmarks - error");
+        }
+    });
+}
+
+function webAPI_getBookmarksNameFiltered (successCallBack, nameString, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL+ "?name="+ nameString +"*",
+        type: 'GET',
+        contentType:'text/plain',
+        data:{},
+        success: bookmarks => {  
+                                
+                                successCallBack(bookmarks);
+                                
+                                console.log("webAPI_getBookmarks - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            errorCallBack(errorThrown);
+            console.log("webAPI_getBookmarks - error");
+        }
+    });
+}
+
+function webAPI_getBookmark( id, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + id,
+        type: 'GET',
+        contentType:'text/plain',
+        data:{},
+        success: bookmark => { successCallBack(bookmark); console.log("webAPI_getBookmark - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            errorCallBack(errorThrown);
+            console.log(bookmark)
+            console.log("webAPI_getBookmark - error");
+        }
+    });
+}
+
+function webAPI_addBookmark( bookmark, successCallBack, errorCallBack) {
+    //console.log("yo")
+    //console.log('add', bookmark)
+    $.ajax({
+        url: apiBaseURL,
+        type: 'POST',
+        contentType:'application/json',
+        data: JSON.stringify(bookmark),
+        success: () => {successCallBack();  console.log("webAPI_addBookmark - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown)
+            errorCallBack(errorThrown);
+            console.log("webAPI_addBookmark - error");
+        }
+    });
+}
+
+function webAPI_modifyBookmark( bookmark, successCallBack, errorCallBack) {
+    console.log('modify', bookmark)
+    $.ajax({
+        url: apiBaseURL + "/" + bookmark.Id,
+        type: 'PUT',
+        contentType:'application/json',
+        data: JSON.stringify(bookmark),
+        success:() => {successCallBack();  console.log("webAPI_modifyBookmark - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            errorCallBack(errorThrown);
+            console.log("webAPI_modifyBookmark - error");
+        }
+    });
+}
+
+function webAPI_deleteBookmark( id, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL+"/" + id,
+        contentType:'text/plain',
+        type: 'DELETE',
+        success:() => {successCallBack();  console.log("webAPI_deleteBookmark - success");},
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown)
+            errorCallBack(errorThrown);
+            console.log("webAPI_deleteBookmark - error");
+        }
+    });
+}
